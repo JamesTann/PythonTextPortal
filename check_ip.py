c91d4cbda857a674a1343d39d439706ae127b72e
JamesTann <jt.21@dartmouth.edu>
2020-08-23 20:12:36 -0400

    Updated IP checker to save existing IP

--- check_ip.py
+++ check_ip.py
@@ -1,21 +1,60 @@
-from urllib import request
 from messaging_service import MessagingService
+import urllib
+import json
+import re
+import os
 
 # using AWS's simple IP checker since it accepts python's default user agent header
 CHECK_IP_URL = 'http://checkip.amazonaws.com'
 
+# save file path
+SAVE_FILE = './ip_save_file.json'
+
 # initiate messaging service
 messager = MessagingService('./config.json')
 
+# get old IP address (if any)
+old_ip = None
+if os.path.isfile(SAVE_FILE):
+    try:
+        # open and parse old IP file
+        with open(SAVE_FILE) as save_file:
+            save = json.load(save_file)
+            old_ip = save.get('ip')
+    except:
+        # send error
+        messager.send_error("Unable to open and parse IP save file.")
+
+external_ip = None
 try:
     # get response
-    response = request.urlopen(CHECK_IP_URL).read()
+    response = urllib.request.urlopen(CHECK_IP_URL).read()
 
     # parse ip from response
-    externalip = response.decode('utf8').split("'")[0].rstrip()
+    external_ip = response.decode('utf8').rstrip()
 
-    # send ip
-    messager.send("External IP: " + externalip)
 except:
     #send error
-    messager.send("Error: Finding and parsing of external IP address failed.")
+    messager.send_error("Finding and parsing of external IP address failed.")
+
+ # pattern to match IP addresses
+ip_pattern = re.compile(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$')
+if external_ip is not None and ip_pattern.match(external_ip):
+
+    # check if IP is new
+    if old_ip is None or old_ip != external_ip:
+
+        # send IP
+        messager.send("New External IP detected: " + external_ip)
+
+        try:
+            # write file
+            with open(SAVE_FILE, 'w') as save_file:
+                save_file.write('{"ip":"' + external_ip + '"}')
+
+        except:
+            # send error
+            messager.send_error("Failed to write new IP save file.")
+else:
+    # send error
+    messager.send_error("Parsed a malformed IP.")
--- messaging_service.py
+++ messaging_service.py
@@ -5,13 +5,25 @@ class MessagingService:
 
     def __init__(self, path):
         self.path = path
-        with open(self.path) as config_file:
-            self.options = json.load(config_file)
-            self.client = Client(self.options.get('account_sid'), self.options.get('auth_token'))
+        
+        # load existing settings and initiate client
+        try:
+            with open(self.path) as config_file:
+                self.options = json.load(config_file)
+                self.client = Client(self.options.get('account_sid'), self.options.get('auth_token'))
+        except:
+            print("Messaging Service Error: Unable to open config file and initiate client.")
 
     def send(self, message):
+
+        # try to send a message
         if self.client is not None and self.options is not None:
-            self.client.messages.create(body=message, from_=self.options.get('twilio_phone_number'), to=self.options.get('personal_phone_number'))
+            try:
+                self.client.messages.create(body=message, from_=self.options.get('twilio_phone_number'), to=self.options.get('personal_phone_number'))
+            except:
+                print("Messaging Service Error: Unable to send message from client.")
         else:
             print("Messaging Service Error: Client or options not initiated.")
 
+    def send_error(self, message):
+        self.send("Error: " + message)
